477a799d5b8ebc538962bbbf298c218a4cb19759
cristian11122008 <73188723+cristian11122008@users.noreply.github.com>
2021-08-30 15:14:48 -0700

    Add files via upload

---
+++ c99.py
@@ -0,0 +1,5 @@
+import os
+import shutil
+source="/home/cristian/Downloads/c97/test.txt"
+destination="/home/cristian/Downloads/c97/test1.txt"
+dest=shutil.copy(source,destination)
---
+++ test.py
@@ -0,0 +1,7 @@
+num=int(input( 'enter your number:'))
+if (num>14):
+    print("your number is too high, guess again")
+elif (num<12):
+    print("your number is too low, guess again")
+else:
+    print("YOU WIN!!!!!!!!!!!!!!!!!!")
